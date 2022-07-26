99bf11d3cd8e72beef9aae267af8e583d7fc229e
aboongm <aboongm@yahoo.com>
2022-07-26 18:32:49 +0530

    Update application_controller.rb, user.rb, new.html.erb (registration), new.html.erb (users)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base 
+    protect_from_forgery with: :exception
+    skip_before_action :verify_authenticity_token
+
+  # prevent users to perfrom action without authentication
+  before_action :authenticate_user!
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected  
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :photo, :bio, :email, :password, :password_confirmation) }
+
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name,:photo, :bio, :email, :password, :current_password) }
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  #  :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
   validates :name, presence: true
   validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: true
+  
+  after_initialize :set_defaults
 
   def self.three_most_recent_posts(author_id)
     Post.where(author_id).order(created_at: :desc).limit(3).all
@@ -16,5 +18,12 @@ class User < ApplicationRecord
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
+  end
+
+
+  private
+
+  def set_defaults
+    self.posts_counter ||= 0
   end
 end
